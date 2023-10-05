1f7356d34737ed528ad0cd825cd270a6741ec612
Vigurskiy Artem <99502996+PoleGlotA@users.noreply.github.com>
2023-10-05 10:55:13 +0300

    Add files via upload

---
+++ Lab_2_1/Lab_2_1/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Text;
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.Write("Введіть розмір масиву N: ");
+        int N = Convert.ToInt32(Console.ReadLine());
+        if (N <= 0)
+        {
+            Console.WriteLine("Помилка: N повинно бути більше 0.");
+            return;
+        }
+        double[] arr = new double[N];
+
+        // Заповнення масиву користувацькими значеннями
+        Console.WriteLine("Введіть елементи масиву:");
+        for (int i = 0; i < N; i++)
+        {
+            Console.Write($"Елемент {i + 1}: ");
+            arr[i] = Convert.ToDouble(Console.ReadLine());
+        }
+
+        Console.Write("Введіть кількість елементів для перестановки M (M < N): ");
+        int M = Convert.ToInt32(Console.ReadLine());
+        if (M >= N)
+        {
+            Console.WriteLine("Помилка: M повинно бути менше за N.");
+            return;
+        }
+        else if(M < 0)
+        {
+            Console.WriteLine("Помилка: M повинно бути більше 0 або дорівнювати йому.");
+            return;
+        }
+
+        // Знайдемо суму непарних елементів масиву
+        double sumOfOddNumbers = 0;
+        foreach (double number in arr)
+        {
+            if (number % 2 != 0)
+            {
+                sumOfOddNumbers += number;
+            }
+        }
+
+        // Знайдемо суму елементів між першим і останнім від'ємними елементами
+        double sumBetweenNegatives = 0;
+        bool foundFirstNegative = false;
+        for (int i = 0; i < N; i++)
+        {
+            if (arr[i] < 0)
+            {
+                if (foundFirstNegative)
+                {
+                    break;
+                }
+                foundFirstNegative = true;
+            }
+            else if (foundFirstNegative)
+            {
+                sumBetweenNegatives += arr[i];
+            }
+        }
+
+        // Переставимо перші M елементів в кінець масиву
+        double[] tempArray = new double[M];
+        Array.Copy(arr, tempArray, M);
+        Array.Copy(arr, M, arr, 0, N - M);
+        Array.Copy(tempArray, 0, arr, N - M, M);
+
+        // Виведемо результати
+        Console.WriteLine($"Сума непарних елементів масиву: {sumOfOddNumbers}");
+        Console.WriteLine($"Сума елементів між першим і останнім від'ємними елементами: {sumBetweenNegatives}");
+        Console.WriteLine("Масив після перестановки перших M елементів в кінець:");
+        foreach (double number in arr)
+        {
+            Console.Write(number + " ");
+        }
+    }
+}
---
+++ Lab_2_2/Lab_2_2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.Write("Введіть кількість рядків матриці: ");
+        int rows = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введіть кількість стовпців матриці: ");
+        int cols = Convert.ToInt32(Console.ReadLine());
+
+        int[,] matrix = new int[rows, cols];
+
+        // Заповнення матриці користувацькими значеннями
+        Console.WriteLine("Введіть елементи матриці:");
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                Console.Write($"Елемент [{i + 1},{j + 1}]: ");
+                matrix[i, j] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+        // Знаходимо добуток елементів в рядках, які не містять від'ємних елементів
+        int productOfRowsWithoutNegatives = 1;
+        for (int i = 0; i < rows; i++)
+        {
+            bool hasNegative = false;
+            for (int j = 0; j < cols; j++)
+            {
+                if (matrix[i, j] < 0)
+                {
+                    hasNegative = true;
+                    break;
+                }
+            }
+            if (!hasNegative)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    productOfRowsWithoutNegatives *= matrix[i, j];
+                }
+            }
+        }
+
+        // Знаходимо максимум серед сум елементів діагоналей, паралельних головній діагоналі матриці
+        int maxSumOfDiagonalElements = int.MinValue;
+        for (int i = 0; i < cols; i++)
+        {
+            int sumDiagonal = 0;
+            for (int j = 0; j < rows && i + j < cols; j++)
+            {
+                sumDiagonal += matrix[j, i + j];
+            }
+            if (sumDiagonal > maxSumOfDiagonalElements)
+            {
+                maxSumOfDiagonalElements = sumDiagonal;
+            }
+        }
+
+        // Виводимо результати
+        Console.WriteLine($"Добуток елементів в рядках без від'ємних елементів: {productOfRowsWithoutNegatives}");
+        Console.WriteLine($"Максимум серед сум елементів діагоналей, паралельних головній діагоналі: {maxSumOfDiagonalElements}");
+    }
+}
